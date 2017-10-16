3ca1428908f0330f18fa81d3fbc6fb56b5a8e407
Spencer Miskoviak <smiskoviak@gmail.com>
2017-10-15 19:29:57 -0700

    switch to postgres

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 20171016022436) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "users", force: :cascade do |t|
     t.string   "name",       null: false
     t.datetime "created_at", null: false
