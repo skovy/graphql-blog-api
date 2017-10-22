929f415ef2740b411f9543ff51f7d55ccbe66293
Spencer Miskoviak <smiskoviak@gmail.com>
2017-10-22 13:39:26 -0700

    add comments migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171022203516) do
+ActiveRecord::Schema.define(version: 20171022203828) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "comments", force: :cascade do |t|
+    t.text     "text",       null: false
+    t.integer  "post_id",    null: false
+    t.integer  "user_id",    null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["post_id"], name: "index_comments_on_post_id", using: :btree
+    t.index ["user_id"], name: "index_comments_on_user_id", using: :btree
+  end
 
   create_table "posts", force: :cascade do |t|
     t.string   "title",      null: false
@@ -30,5 +40,7 @@ ActiveRecord::Schema.define(version: 20171022203516) do
     t.datetime "updated_at", null: false
   end
 
+  add_foreign_key "comments", "posts"
+  add_foreign_key "comments", "users"
   add_foreign_key "posts", "users"
 end
